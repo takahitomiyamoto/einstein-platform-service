426f3772af3f612014e171b370c013dc3996fa9b
takahito.miyamoto <takahito.miyamoto@accenture.com>
2020-02-06 22:56:48 +0900

    :sparkles:

---
+++ scripts/shell/initNodeModules.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+echo
+echo ----------------------------------------
+echo "install sfdx and yarn globally"
+echo ----------------------------------------
+npm install -g sfdx-cli yarn
+
+echo
+echo ----------------------------------------
+echo "install sfpowerkit"
+echo ----------------------------------------
+echo "y" | sfdx plugins:install sfpowerkit
+
+echo
+echo ----------------------------------------
+echo "update devDependencies"
+echo ----------------------------------------
+yarn add --dev --exact \
+@babel/core \
+@babel/preset-env \
+@babel/register \
+@prettier/plugin-xml \
+@salesforce/eslint-config-lwc \
+eslint \
+gulp \
+gulp-shell \
+husky \
+lint-staged \
+prettier \
+prettier-plugin-apex
+
+echo
+echo ----------------------------------------
+echo "install node_modules"
+echo ----------------------------------------
+yarn install
